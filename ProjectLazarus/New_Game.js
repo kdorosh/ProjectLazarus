505736b70149a815392e51f033a65938f6665e9b
Jordan <jtk296@gmail.com>
2015-10-06 00:00:08 -0400

    added general framework for the game

---
+++ ProjectLazarus/New_Game.js
@@ -0,0 +1,25 @@
+var boundsX = 800, boundsY = 600;
+var game = new Phaser.Game(boundsX, boundsY, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+//the player
+var shep;
+//the enemy chasing shep
+var reap;
+
+var background;
+
+function preload(){
+
+
+}
+
+function create(){
+
+
+
+}
+
+function update(){
+
+
+}
---
+++ ProjectLazarus/src/preload.js
@@ -0,0 +1,17 @@
+var preload = function(game) {}
+
+preload.prototype= {
+	preload: function() {
+		//cretes a loading bar
+		var loadingBar = this.add.sprite(160, 240,"loading");
+		loadingBar.anchor.setTo(0.5,0.5);
+		this.load.setPreloadSprite(loadingBar);
+		//in here we load all the assets we will be using to make the game
+		this.game.load.image("blahblah","img.blahblah.png");
+		//etc. etc. etc. etc.
+	},
+	create: function(){
+		// will start the game title function
+		this.game.state.start(GameTitle);
+	}
+}
