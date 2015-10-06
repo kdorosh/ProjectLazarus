4b99f7fc1ae2da4a1c6c65cf17da5956b2a433d6
kevdliu <kevdliu@gmail.com>
2015-10-06 01:11:43 -0400

    initial normandy loading

---
+++ src/normandy.js
@@ -0,0 +1,48 @@
+function Normandy(game) {
+	this.game = game;
+} 
+
+Normandy.prototype.createPlayer = function() {
+  this.object = this.game.add.sprite(50, 50, 'normandy');
+  this.object.scale.setTo(0.5, 0.5);
+  this.object.anchor.setTo(0.5, 0.5);
+  game.physics.arcade.enable(this.object);
+}
+  
+/**
+function updatePlayer() {
+    var mX = game.input.mousePointer.x;
+    var mY = game.input.mousePointer.y;
+    pointAt(mX, mY);
+
+    if (wasd.up.isDown) {
+        moveY(-3);
+    }
+    if (wasd.down.isDown) {
+        moveY(3);
+    }
+    if (wasd.left.isDown) {
+        moveX(-3);
+    }
+    if (wasd.right.isDown) {
+        moveX(3);
+    }
+}
+
+function moveX(deltaX) {
+	ship.x += deltaX;
+}
+
+function moveY(deltaY) {
+	ship.y += deltaY;
+}
+
+function pointAt(x, y) {
+	ship.angle = Math.atan2(ship.position.x - x, ship.position.y - y)  * -57.2957795;
+}
+
+function killPlayer(ship, enemy) {
+	ship.kill();
+}
+*/
+
